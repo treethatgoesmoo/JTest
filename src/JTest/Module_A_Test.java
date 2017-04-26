9759231585add82e62c45fa74fd80a103f2fdfff
Robin <treethatgoesmoo@gmail.com>
2017-04-26 00:49:52 -0700

    first

--- src/JTest/Module_A_Test.java
+++ src/JTest/Module_A_Test.java
@@ -10,7 +10,7 @@ public class Module_A_Test {
 	public void test(){
 		Module_A module_a = new Module_A();
 		
-		assertEquals("This should return 1", 1, module_a.return_num());
+		assertEquals("This should return 1", -1, module_a.return_num());
 		
 	}
 	
